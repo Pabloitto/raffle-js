8f13ebb955c6271a38872b3a1ecc0805d4f492f2
Pablo Itto <pablo.itto@unosquare.com>
2018-07-13 15:44:28 -0700

    add default port

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.use('/', express.static('public', {
   index: 'main.html'
 }))
 
-app.listen(3000, function () {
-  console.log('Server listening on port 3000')
+app.set('port', (process.env.PORT || 3000))
+app.listen(app.get('port'), () => {
+  console.log('App is running on port', app.get('port'))
 })
